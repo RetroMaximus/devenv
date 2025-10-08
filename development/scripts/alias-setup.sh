bc51c192155c34e7cb64f00ec7290f0e09845ca8
RFPSystems Production Applications <59292329+RetroMaximus@users.noreply.github.com>
2025-10-08 05:14:22 -0400

    Add files via upload

--- development/scripts/alias-setup.sh
+++ development/scripts/alias-setup.sh
@@ -1,10 +1,12 @@
-echo "alias dev='~/devenv/development/scripts/setup-dev-env.sh'" >> ~/.bashrc
-echo "alias projects='~/devenv/development/scripts/project-manager.sh'" >> ~/.bashrc
-echo "alias config-dev='~/devenv/development/scripts/config-manager.sh'" >> ~/.bashrc
+echo "alias dev" "~/devenv/development/scripts/setup-dev-env.sh" >> ~/.bashrc
+echo "alias  projects" "~/devenv/development/scripts/project-manager.sh" >> ~/.bashrc
+echo "alias  batch-import" "~/devenv/development/scripts/batch-import-projects.sh" >> ~/.bashrc
+echo "alias  config-dev" "~/devenv/development/scripts/config-manager.sh" >> ~/.bashrc
+echo "alias  lang-setup" "~/devenv/development/scripts/language-manager.sh" >> ~/.bashrc
+echo "alias  set-streamer-mask" "~/devenv/development/scripts/assign-streamer-mask.sh" >> ~/.bashrc
+echo "alias  set-ip-mask" "~/devenv/development/scripts/assign-ip-mask.sh" >> ~/.bashrc
+echo "alias  set-streamer-mode" "~/devenv/development/scripts/assign-streamer-mode.sh" >> ~/.bashrc
+echo "alias  show-streamer-status" "~/devenv/development/scripts/show-streamer-mode-status.sh" >> ~/.bashrc
+
 source ~/.bashrc
 
-# Terminal Alias Commands
-#
-# dev
-# projects
-# config-dev
